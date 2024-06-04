150b77672b167cb353382078eb684f2a1ee11c68
9509298U <robin.lehuerou-kerisel@sncf.fr>
2024-06-04 17:26:19 +0200

    two notebooks on datadrift and explainability

--- src/ocp7_scoring_model_cloud/Model_API/main.py
+++ src/ocp7_scoring_model_cloud/Model_API/main.py
@@ -7,6 +7,7 @@ import pandas as pd
 import sklearn
 import os
 import uvicorn
+import shap
 
 # Define the input data model
 class InputData(BaseModel):
@@ -19,6 +20,7 @@ app = FastAPI()
 base_dir = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(base_dir, "production_model")
 model = mlflow.sklearn.load_model(model_path)
+explainer = shap.Explainer(model)
 
 @app.get("/")
 def read_root():
@@ -36,6 +38,20 @@ async def predict(input_data: InputData): # we could use InputData with a Pydant
 
         # Return the prediction as a response
         return {"prediction": prediction.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/explain_local")
+async def shap_values(input_data: InputData): # we could use InputData with a Pydantic model based on mlflow signature
+    try:
+        # Convert input data to DataFrame
+        data_df = pd.DataFrame(input_data.dataframe_records)
+
+        # Get prediction from the model
+        shap_values = explainer(data_df)
+
+        # Return the prediction as a response
+        return {"shap_values": shap_values.values.tolist()}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
